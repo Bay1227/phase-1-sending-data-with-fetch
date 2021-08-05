b54a4e1fead1ca5a7a522ed0ad621cfe61eed7a2
Bayartsengel Battulga <tsengelbattulga1227@gmail.com>
2021-08-04 22:25:14 -0700

    Completed Fetch

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Add your code here
+const submitData = function(userName, userEmail) {
+    return fetch("http://localhost:3000/users",{
+        method:"POST",
+        headers: {
+            "Content-Type" : "application/json",
+            "Accept" : "application/json"
+        },
+        body: JSON.stringify({name: "Steve", email: "steve@steve.com"})
+    })
+    .then((res) => res.json())
+    .then(function(obj){
+        const newId = document.createElement('div');
+        newId.innerHTML = obj.id;
+        document.body.append(newId);
+    })
+    .catch((error) => {
+        alert("Error");
+        //console.log(error.message);
+        const newEr = document.createElement('span')
+        newEr.innerHTML = error;
+        document.body.append(newEr)
+})
+}
+
+
